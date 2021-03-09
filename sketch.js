924a6ab1a3af053aa429cfec668862ac572c85b8
tathagat12345 <80099872+tathagat12345@users.noreply.github.com>
2021-03-09 15:44:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var hr,mn,sc,hr_angle,mn_angle,sc_angle;
+function setup() {
+  createCanvas(400,400);
+ angleMode(DEGREES)
+
+}
+
+function draw() {
+  background(0,0,0); 
+  translate(200,200) 
+  rotate(-90)
+  hr=hour();
+  mn=minute();
+  sc=second();
+sc_angle=map(sc,0,60,0,360);
+mn_angle=map(mn,0,60,0,360);
+hr_angle=map(hr%12,0,12,0,360);
+ push();
+ rotate(sc_angle);
+ stroke(255,0,0);
+ strokeWeight(7);
+ line(0,0,100,0);
+ pop();
+ push();
+ rotate(mn_angle);
+ stroke(0,255,0);
+ strokeWeight(7);
+ line(0,0,75,0);
+ pop();
+ push();
+ rotate(hr_angle);
+ stroke(0,0,255);
+ strokeWeight(7);
+ line(0,0,50,0);
+ pop();
+ strokeWeight(10);
+ noFill();
+ stroke(255,0,0);
+ arc(0,0,300,300,0,sc_angle);
+ stroke(0,255,0);
+ arc(0,0,280,280,0,mn_angle);
+ stroke(0,0,255);
+ arc(0,0,250,250,0,hr_angle);
+  drawSprites();
+}
